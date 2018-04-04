ceb2951be142e5034b580b8a24ce2a3f1e5e6eb7
Edyta <dudek002@hotmail.com>
2018-04-04 15:09:26 -0500

    my news added

---
+++ server.js
@@ -0,0 +1,149 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var request = require("request");
+var cheerio = require("cheerio");
+
+var Promise = require("bluebird");
+
+mongoose.Promise = Promise;
+
+var PORT = 3000;               
+
+// Require all models
+var db = require("./models");
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+
+mongoose.connect("mongodb://localhost/week18Populater", {
+  useMongoClient: true
+});
+
+// Routes
+
+// A GET route for scraping the echojs website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("http://people.com/news/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h3").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.headline = $(this)
+        .children("a")
+        .text();
+      result.URL = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Headline using the `result` object built from scraping
+      db.Headline.create(result)
+        .then(function(dbHeadline) {
+          // View the added result in the console
+          console.log(dbHeadline);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });
+
+    /*$("article a").each(function(i, element) {
+      // Save an empty result object
+      var resultImg = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      resultImg.image = $(this)
+        .children("img")
+        .attr("src");
+
+      // Create a new Headline using the `result` object built from scraping
+      db.Headline.create(resultImg)
+        .then(function(dbHeadline) {
+          // View the added result in the console
+          console.log(dbHeadline);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });*/
+
+    // If we were able to successfully scrape and save an Headline, send a message to the client
+    res.send("Scrape Complete");
+  });
+});
+
+// Route for getting all Headlines from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Headline.find({})
+    .then(function(dbHeadline) {
+      // If we were able to successfully find Headlines, send them back to the client
+      res.json(dbHeadline);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Headline by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Headline.findById(req.params.id)
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbHeadline) {
+      // If we were able to successfully find an Headline with the given id, send it back to the client
+      res.json(dbHeadline);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Headline's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Headline with an `_id` equal to `req.params.id`. Update the Headline to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Headline.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbHeadline) {
+      // If we were able to successfully update an Headline, send it back to the client
+      res.json(dbHeadline);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
